168111fc6edeacdb7f69274c1be1b657a05effea
aniede <ndubest56@gmail.com>
2023-03-09 16:17:25 +0100

    pointer printing function

---
+++ print_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char printcar(char *str);
+
+main()
+{
+	char stringer[] = "I love my wife so much";
+
+	char *pts_str;
+	pts_str = stringer;
+
+	pts_str = "I want to know alot about C";
+
+	printcar(stringer);
+	printcar(pts_str);
+
+	return (0);
+}
+char printcar(char *str)
+{
+	printf("%s\n", str);
+}
