cc81aa71bfb6b5075810dfbc3217a55a12c8da0e
aniede <ndubest56@gmail.com>
2023-03-17 18:45:13 +0100

    struct function with typedef

---
+++ struct_function.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+struct automobile{
+                int year;
+                char make[10];
+                int engine_power;
+                float cost;
+        } merc, ikenga, ikendu, benz;
+
+        struct automobile info = {
+                1990,
+                "mk2020",
+                8500,
+                1500
+        };
+
+typedef struct automobile STA;
+
+STA structfunc(STA s);
+
+int main(void)
+{
+        STA model;
+
+
+
+
+        printf("The initial values for directions: \n");
+        printf("Year: %d \n", info.year);
+        printf("Car make: %s \n", info.make);
+        printf("Car Engine power: %d \n", info.engine_power);
+        printf("cost: $%6.2f \n\n", info.cost);
+
+        model = structfunc(model);
+
+        printf("The details of your car are: \n");
+
+        structfunc(model);
+
+        printf("Year: %d\n", model.year);
+        printf("Make: %s\n", model.make);
+        printf("Cost: $%0.2f\n", model.cost);
+        printf("Car Engine power: %dmkg\n", model.engine_power);
+
+
+            return (0);
+}
+STA structfunc(STA s)
+{
+         printf("Which year did you bought Ikendu car?\n");
+          scanf("%d", &s.year);
+        printf("What is the make?\n");
+          scanf("%s", &s.make);
+        printf("What is the cost of the car?\n");
+          scanf("%f", &s.cost);
+        printf("What is the EngPower of the car?\n");
+          scanf("%d", &s.engine_power);
+
+        return s;
+}
+
+ /*  struct automobile{
+                int year;
+                char make[10];
+                int engine_power;
+                float cost;
+        } info = {
+        1990,
+        "mk2020",
+        8500,
+        1500};
+        */
--- structure.c
+++ structure.c
@@ -8,7 +8,7 @@ int main()
                 int year;
                 char make[10];
                 int engine_power;
-                float weight;
+                float cost;
         } merc, ikenga, ikendu, benz;
 
         struct automobile info = {
@@ -18,25 +18,40 @@ int main()
                 1500
         };
 
+       /*  struct automobile{
+                int year;
+                char make[10];
+                int engine_power;
+                float cost;
+        } info = {
+        1990,
+        "mk2020",
+        8500,
+        1500};
+        */
+
         printf("The initial values for directions: \n");
         printf("Year: %d \n", info.year);
         printf("Car make: %s \n", info.make);
         printf("Car Engine power: %d \n", info.engine_power);
-        printf("weight: %6.2f \n\n", info.weight);
+        printf("cost: $%6.2f \n\n", info.cost);
 
 
         printf("Which year did you bought Ikendu car?\n");
           scanf("%d", &ikendu.year);
         printf("What is the make?\n");
           scanf("%s", &ikendu.make);
-        printf("What is the weight of the car?\n");
-          scanf("%f", &info.weight);
+        printf("What is the cost of the car?\n");
+          scanf("%f", &info.cost);
+        printf("What is the EngPower of the car?\n");
+          scanf("%d", &info.engine_power);
 
         printf("The details of your car are: \n");
 
         printf("Year: %d\n", ikendu.year);
         printf("Make: %s\n", ikendu.make);
-        printf("Weight: %0.2f\n", info.weight);
+        printf("Cost: $%0.2f\n", info.cost);
+        printf("Car Engine power: %d \n", info.engine_power);
 
 
             return (0);
