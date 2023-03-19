c1fab44c34fce9d621f5fbfb1082efa1e49078bb
aniede <ndubest56@gmail.com>
2023-03-19 15:33:04 +0100

    still on fopen fgetc fputc

--- fopen_read.c
+++ fopen_read.c
@@ -5,21 +5,27 @@ int main()
         FILE *fptr1, *fptr2;
         int end = 0;
 
+        char filen[] = "text.txt";
+        char filen1[] = "text2.txt";
+
         if ((fptr1 = fopen(filen, "r")) == NULL);
         {
                 printf("fopen function failed \n");
                 end = 1;
         }
-        if ((fptr2 = fopen(file, "w")) == NULL);
+        if ((fptr2 = fopen(filen1, "w")) == NULL);
         {
                 printf("fopen function failed \n");
                 end = 1;
         }
+        readWrite(fptr1, fptr2);
+        fclose(fptr1);
+        fclose(fptr2);
 
         return end;
 
 }
-void readWrite(FILE f1, FILE f2)
+void readWrite(FILE *f1, FILE *f2)
 {
         char c;
         while ((c = fgetc(f1)) != EOF)
