52be836f914a74b492301a70657814e300df1da4
aniede <ndubest56@gmail.com>
2023-03-19 20:10:49 +0100

    working on block file reading and writing

--- fopen_block.c
+++ fopen_block.c
@@ -9,9 +9,17 @@ int main()
         FILE *ptrf1, *ptrf2;
         int end = 0;
 
-        if ((ptrf1 = (fopen("text.txt", "w")) == NULL)
+        if ((ptrf1 = fopen("text.txt", "w")) == NULL)
         {
                 printf("The fopen did not succeed \n");
                 end = 1
         }
+        else if ((ptf2 = fopen("text1.txt", "r")) == NULL)
+        {
+                printf("The 2nd fopen function failed \n");
+                end = 1;
+        }
+        fclose(ptrf1);
+        fclose(ptrf2);
+        return (end);
 }
