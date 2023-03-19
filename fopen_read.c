6b217e63f6452170bdc25352da761478f5e86c35
aniede <ndubest56@gmail.com>
2023-03-19 16:16:41 +0100

    finalizing the fgetc fputc

--- fopen_read.c
+++ fopen_read.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 
+void readWrite(FILE *f1, FILE *f2);
 int main()
 {
         FILE *fptr1, *fptr2;
         int end = 0;
 
-        char filen[] = "text.txt";
-        char filen1[] = "text2.txt";
+        char filen[] = "text2.txt";
+        char filen1[] = "text.txt";
 
-        if ((fptr1 = fopen(filen, "r")) == NULL);
+        if ((fptr1 = fopen(filen, "w")) == NULL)
         {
                 printf("fopen function failed \n");
                 end = 1;
         }
-        if ((fptr2 = fopen(filen1, "w")) == NULL);
+        else if ((fptr2 = fopen(filen1, "r")) == NULL)
         {
-                printf("fopen function failed \n");
+                printf("fopen II function failed \n");
                 end = 1;
         }
-        readWrite(fptr1, fptr2);
-        fclose(fptr1);
-        fclose(fptr2);
+        else
+        {
+                readWrite(fptr2, fptr1);
+                fclose(fptr1);
+                fclose(fptr2);
+        }
+
 
         return end;
 
 }
 void readWrite(FILE *f1, FILE *f2)
 {
-        char c;
+        int c;
+
         while ((c = fgetc(f1)) != EOF)
         {
                 fputc(c, f2);
