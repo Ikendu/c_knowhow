8c2e6cc8f2789c283c1f2a4c4caf65fde2fe3586
aniede <ndubest56@gmail.com>
2023-03-29 13:53:02 +0100

    normal bubble sorting

--- funcPointSort.c
+++ funcPointSort.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
-int compare(int a, int b)
+int BubbleSort(int A[], int n)
+{
+        int i, j, temp;
+
+        for (i = 0; i < n; i++)
+            for (j = 0; j < (n-1); j++)
+            {
+                    if (A[j] > A[j+1])
+                    {
+                            temp = A[j];
+                            A[j] = A[j + 1];
+                            A[j + 1] = temp;
+                    }
+            }
+
+}
+int main()
+{
+        int i, A[] = {2, 9, 10, 44, 20, 7};
+        BubbleSort(A, 6);
+
+        for (i = 0; i < 6; i++)
+        printf("%d ", A[i]);
+}
+/* int compare(int a, int b)
 {
         if (a > b) return 1;
         else return -1;
 }
 
-int BubbleSort(int A[], int n, int (*compare)(int, int))
+int BubbleSort(int *A, int n, int (*compare)(int, int))
 {
         int i, j, temp;
 
@@ -31,3 +55,4 @@ int main()
         for(i > 0; i < 6; i++)
         printf("%d ", A[i]);
 }
+*/
