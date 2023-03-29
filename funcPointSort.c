4b6cf5c3b5a8a1b7e0c3efd44e426a5e4f2364c6
aniede <ndubest56@gmail.com>
2023-03-29 14:21:59 +0100

    sorting backwards

--- funcPointSort.c
+++ funcPointSort.c
@@ -1,58 +1,26 @@
 #include <stdio.h>
 
-int BubbleSort(int A[], int n)
-{
-        int i, j, temp;
-
-        for (i = 0; i < n; i++)
-            for (j = 0; j < (n-1); j++)
-            {
-                    if (A[j] > A[j+1])
-                    {
-                            temp = A[j];
-                            A[j] = A[j + 1];
-                            A[j + 1] = temp;
-                    }
-            }
-
-}
-int main()
-{
-        int i, A[] = {2, 9, 10, 44, 20, 7};
-        BubbleSort(A, 6);
-
-        for (i = 0; i < 6; i++)
-        printf("%d ", A[i]);
-}
-/* int compare(int a, int b)
+//bubble sort with function pointers
+int compare(int a, int b)
 {
         if (a > b) return 1;
         else return -1;
 }
-
-int BubbleSort(int *A, int n, int (*compare)(int, int))
+int BubbleSort(*A, int n, (*compare)(int, int))
 {
         int i, j, temp;
 
-        for (i = 0; i < n; i++)
-            for (j = 0; j < (n - 1); j++)
+        for (i = n; i < n; i++)
+            for (j = n; j < (n - 1); j++)
+            if (compare(A[j], A[j + 1]) > 0)
             {
-                    if (compare(A[j], A[j+1]) > 0)
-                    {
-                            temp = A[j];
-                            A[j] = A[j+1];
-                            A[j+1] = temp;
-                    }
+                    temp = A[j];
+                    A[j] = A[j+1];
+                    A[j+1] = temp;
             }
 }
 
 int main()
 {
-        int i, A[] = {2, 5, 9, 10, 8, 1};
-
-        BubbleSort(A, 6, compare);
-
-        for(i > 0; i < 6; i++)
-        printf("%d ", A[i]);
+        
 }
-*/
