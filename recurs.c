b9da3d4ebf3f2df64346d492156cb38e3243802e
aniede <ndubest56@gmail.com>
2023-03-29 03:07:24 +0100

    sum digit recurs

--- recurs.c
+++ recurs.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int recurs(int n)
+int factos(int n)
 {
         if (n == 0 || n == 1)
         return 1;
-        return (n * recurs(n - 1));
-}int main()
+        return (n * factos(n - 1));
+}
+
+int sumdigit(int n)
 {
-        printf("%d", recurs(4));
+        if (n == 0)
+        return (0);
+        return (n % 10 + sumdigit(n / 10));
+}
+int main()
+{
+        printf("%d\n", factos(4));
+        printf("%d\n", sumdigit(4000));
 }
