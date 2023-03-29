66ec8ce9311baac86caa7afe1d28dfaaa948f0e5
aniede <ndubest56@gmail.com>
2023-03-29 03:43:25 +0100

    dynamic memory and static mem allocation

---
+++ dynamicStaticMem.c
@@ -0,0 +1,18 @@
+/*
+Dynamic Memory Allocation and Static Memory Allocation
+In static memory allocation:
+1. memory is allocated from stack
+2. memory is allocated at compile time
+3. memory cannot be increased or decreased (fixed)
+4. memory cannot be freed.
+5. normal or pointers can be used
+
+In dynanmic memeory alloction
+1. memory s allocated from heap
+2. memory is allocated at runtime
+3. memory can be increased or decreased
+4. memory can be freed and reused at any point of the program
+5. There is no wastage of memory
+6. pointer only variables are used
+7. malloc, calloc, realloc and free fuctions are used
+*/
