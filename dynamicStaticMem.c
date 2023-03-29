c80b51ebbaa9f6ecb1c77d60d1b9b99bf4c9d25a
aniede <ndubest56@gmail.com>
2023-03-29 10:40:20 +0100

    malloc allocation revisit

--- dynamicStaticMem.c
+++ dynamicStaticMem.c
@@ -14,5 +14,34 @@ In dynanmic memeory alloction
 4. memory can be freed and reused at any point of the program
 5. There is no wastage of memory
 6. pointer only variables are used
-7. malloc, calloc, realloc and free fuctions are used
+7. malloc, calloc, realloc allocates memory while free deallocate memory
+
+
+malloc returns a void pointer that gives us the address of the first byte in block
+of memory tat it allocate.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int n, i;
+
+        printf("Enter the size of the array \n");
+        scanf("%d", &n);
+
+        printf("The size u enter is %d and the array contains \n", n);
+
+        int *arr = malloc(n * sizeof(int));
+        for ( i = 0; i < n; i++)
+        {
+                arr[i] = i + 1;
+        }
+
+        for (i = 0; i < n; i++)
+        {
+                printf(" %d ", arr[i]);
+        }
+
+}
