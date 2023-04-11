6e2a7b28b668bfedcae5951d6e17d35384c82ddf
aniede <ndubest56@gmail.com>
2023-04-11 17:59:38 +0100

    more on array verses linked list insertions

--- list_arraypos.c
+++ list_arraypos.c
@@ -4,17 +4,13 @@
 
 int insert_pos(int arr[], int arr2[], int n, int data, int pos)
 {
-        int i;
-
-        int index = pos-1;
+        int i, j, index = pos-1;
 
         for (i = 0; i <= index-1; i++)
         {
                 arr2[i] = arr[i];
         }
         arr2[index] = data;
-
-        int j;
 
         for (i = index+1, j = index; i < n+1, j < n; i++, j++)
         {
@@ -28,7 +24,8 @@ int main()
         int i;
         int size = sizeof(arr)/sizeof(arr[0]);
         int arr2[size+1];
-        insert_pos(arr, arr2, size, 100, 5);
+        
+        insert_pos(arr, arr2, size, 100, 9);
 
         for (i = 0; i < size+1; i++)
         {
