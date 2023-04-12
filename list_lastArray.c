c7d8fa3ad2edaf3ee26160b1a72c646ae7954a0f
aniede <ndubest56@gmail.com>
2023-04-12 15:47:24 +0100

    deleting last element of an array

---
+++ list_lastArray.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//deleting the last element of an array
+
+int main()
+{
+        int arry[] = {60, 50, 10, 3, 60, 73};
+        int i;
+        int size = sizeof(arry)/sizeof(arry[0]);
+        size = size-1;
+
+        for (i = 0; i < size; i++)
+        printf("%d \n", arry[i]);
+}
