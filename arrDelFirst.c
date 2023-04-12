dc0fb08cc6721fcdbe1b3623443e76f26dea1109
aniede <ndubest56@gmail.com>
2023-04-13 00:05:12 +0100

    working on delete array version

--- arrDelFirst.c
+++ arrDelFirst.c
@@ -10,7 +10,25 @@ int del_first(int arr[], int n)
         }
         for (i = 0; i < n-1; i++)
         {
-                a[i] = a[i + 1];
+                arr[i] = arr[i + 1];
         }
         return (n - 1);
+}
+int main()
+{
+        int i, n, arr[5];
+
+        printf("Enter the number of elements < 6\n");
+        scanf("%d", &n);
+
+        printf("Enter the element one after the other\n");
+        for (i = 0; i < n; i++)
+        {
+                scanf("%d \n", &arr[i]);
+        }
+        for(i = 0; i < n-1; i++)
+        {
+                printf("%d\n", arr[i]);
+        }
+        return (0);
 }
