745ea76f467c3a519b8553edd2ecb568d0a8a7d2
aniede <ndubest56@gmail.com>
2023-04-21 09:43:37 +0100

    shell basic

---
+++ myshell.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char *cmd = NULL, *cmd_cpy = NULL, *token = NULL;
+	char *delim = " \n";
+	size_t n = 0;
+	int argc = 0, i = 0;
+	char **argv = NULL;
+
+	printf("$ ");
+
+	if (getline(&cmd, &n, stdin) == -1)
+		return -1;
+
+	cmd_cpy = strdup(cmd);
+	
+	while (cmd[i])
+	{
+		if (cmd[i] == '\n')
+			printf("we have %ld character here", strlen(cmd));
+		else
+			printf("%c", cmd[i]);
+		i++;
+	}
+	return (0);
+}
